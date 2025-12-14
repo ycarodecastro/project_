01e97b9deecb8997731dbd3ae781d9a6faa16ae0
ycarodecastro <ycarodecastro13@gmail.com>
2025-12-13 22:45:19 -0300

    parte quatro

--- app/models/userProfileModel.py
+++ app/models/userProfileModel.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from usersModel import Users
 
 Base = declarative_base()
 
