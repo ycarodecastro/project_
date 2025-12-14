119e2a55d7265726e38f26aefaf90514c6929f98
ycarodecastro <ycarodecastro13@gmail.com>
2025-12-13 23:42:40 -0300

    12

--- app/database.py
+++ app/database.py
@@ -7,3 +7,6 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
+
