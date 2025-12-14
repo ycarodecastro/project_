b58476187e612ded2082a2f1a82bd9b7a9608d06
ycarodecastro <ycarodecastro13@gmail.com>
2025-12-14 00:14:46 -0300

    corrigindo o erro do crypt

--- app/security.py
+++ app/security.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    # truncar para 72 bytes se quiser, ou deixar o passlib cuidar
+    password_bytes = password.encode('utf-8')[:72]
+    return pwd_context.hash(password_bytes.decode('utf-8'))
 
 def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
