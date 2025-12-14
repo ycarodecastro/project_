bf3ef04a6920dc78d3356c0cbfada66f4446827b
ycarodecastro <ycarodecastro13@gmail.com>
2025-12-13 23:53:33 -0300

    afaf

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Conexão PostgreSQL
-DATABASE_URL = "postgresql://dbproduct_zfda_user:xEP9aNQpvXGXQtN3FX1PCPxTOoLPp0ga@dpg-d4ut4adactks73fa1k60-a.ohio-postgres.render.com/dbproduct_zfda"
+DATABASE_URL = "postgresql://dbprofiles_user:4JReo5cKlwKs1ja5gYdpQl5oJtDEOy4J@dpg-d4v2chali9vc73dfm5fg-a.ohio-postgres.render.com/dbprofiles"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
