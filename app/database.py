52d6eeef31eacaec4d9c4443edf77a3ca567cc1b
ycarodecastro <ycarodecastro13@gmail.com>
2025-12-16 00:37:49 -0300

    finalmnete terminado

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Conexão PostgreSQL
-DATABASE_URL = "postgresql://dbprincipal_user:Pc3O8hOpHog0VIV4zXtChim6lkGpydOH@dpg-d4voih7pm1nc73bsnsig-a.ohio-postgres.render.com/dbprincipal"
+DATABASE_URL = "postgresql://dbprincipal2_user:Rzf5M8K0nP6gTXnwCbbWsQCNoEzfWYA3@dpg-d50d7gt6ubrc739qsmug-a.ohio-postgres.render.com/dbprincipal2"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
